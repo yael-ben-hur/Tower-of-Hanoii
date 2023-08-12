2b56953fd2a1d6b49bf1ffc5860c814abfe4a4b6
yael-ben-hur <yaelbh2904@gmail.com>
2023-08-13 01:52:52 +0300

    Add project files.

---
+++ WindowsFormsApp40/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsApp40
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 637);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(123, 620);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Choose the number of hoops (between 3-9) that you want in the tower then press he" +
+    "re";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(490, 634);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Movement";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(604, 634);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp40.Properties.Resources.תמונה9;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1174, 690);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "מגדלי הנוי";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp40/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp40
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int l, g, size, i=0, t=0;
+        int []tower;
+        int[] moveto;
+        int[] whereisthetowerpart;
+        Panel [] towerpic;
+        int sizex=300, sizey=54, locationx=61, locationy=520, subfromsize, savelx, savely, checkwhere, whichpart, height;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        Image player = Properties.Resources.תמונה7;
+        private void SolveHanoi(int n)
+        {
+            SolveHanoi(1, 2, 3, n);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (i == 0)
+                return;
+            else
+            {
+                height = 0;
+                if (t<moveto.Length&& moveto[t]==1)
+                {
+                    checkwhere = moveto[t];
+                    for (int p = whereisthetowerpart.Length-2; p>-1; p=p-2)
+                    {
+                        if (whereisthetowerpart[p]==checkwhere)
+                        {
+                            whichpart = whereisthetowerpart[p + 1];
+                            whereisthetowerpart[p] = moveto[t + 1];
+                            break;
+                        }
+                    }
+                    t++;
+                    if (moveto[t]==3)
+                    {
+                        locationx = savelx + (subfromsize / 2) * whichpart + 759;
+                        for (int p = 0; p<whichpart*2; p=p+2)
+                        {
+                          if (whereisthetowerpart[p]==moveto[t])
+                            {
+                                height++;
+                            }
+                        }
+                        locationy = savely - sizey * height;
+                        if (whichpart<towerpic.Length)
+                        {
+                            towerpic[whichpart].Location = new Point(locationx, locationy);
+                        }
+                        t++;
+                        if (t == moveto.Length)
+                            this.Controls.Remove(button2);
+                        return;
+                    }
+                    if (moveto[t] == 2)
+                    {
+                        locationx = savelx + (subfromsize / 2) * whichpart + 380;
+                        for (int p = 0; p < whichpart * 2; p = p + 2)
+                        {
+                            if (whereisthetowerpart[p] == moveto[t])
+                            {
+                                height++;
+                            }
+                        }
+                        locationy = savely - sizey * height;
+                        if (whichpart < towerpic.Length)
+                        {
+                            towerpic[whichpart].Location = new Point(locationx, locationy);
+                        }
+                        t++;
+                        if (t == moveto.Length)
+                            this.Controls.Remove(button2);
+                        return;
+                    }
+                }
+                if (t < moveto.Length && moveto[t] == 2)
+                {
+                    checkwhere = moveto[t];
+                    for (int p = whereisthetowerpart.Length - 2; p > -1; p = p - 2)
+                    {
+                        if (whereisthetowerpart[p] == checkwhere)
+                        {
+                            whichpart = whereisthetowerpart[p + 1];
+                            whereisthetowerpart[p] = moveto[t + 1];
+                            break;
+                        }
+                    }
+                    t++;
+                    if (moveto[t] == 3)
+                    {
+                        locationx = savelx + (subfromsize / 2) * whichpart + 759;
+                        for (int p = 0; p < whichpart * 2; p = p + 2)
+                        {
+                            if (whereisthetowerpart[p] == moveto[t])
+                            {
+                                height++;
+                            }
+                        }
+                        locationy = savely - sizey * height;
+                        if (whichpart < towerpic.Length)
+                        {
+                            towerpic[whichpart].Location = new Point(locationx, locationy);
+                        }
+                        t++;
+                        if (t == moveto.Length)
+                            this.Controls.Remove(button2);
+                        return;
+                    }
+                    if (moveto[t] == 1)
+                    {
+                        locationx = savelx + (subfromsize / 2) * whichpart ;
+                        for (int p = 0; p < whichpart * 2; p = p + 2)
+                        {
+                            if (whereisthetowerpart[p] == moveto[t])
+                            {
+                                height++;
+                            }
+                        }
+                        locationy = savely - sizey * height;
+                        if (whichpart < towerpic.Length)
+                        {
+                            towerpic[whichpart].Location = new Point(locationx, locationy);
+                        }
+                        t++;
+                        if (t == moveto.Length)
+                            this.Controls.Remove(button2);
+                        return;
+                    }
+                }
+                if (t < moveto.Length && moveto[t] == 3)
+                {
+                    checkwhere = moveto[t];
+                    for (int p = whereisthetowerpart.Length - 2; p > -1; p = p - 2)
+                    {
+                        if (whereisthetowerpart[p] == checkwhere)
+                        {
+                            whichpart = whereisthetowerpart[p + 1];
+                            whereisthetowerpart[p] = moveto[t + 1];
+                            break;
+                        }
+                    }
+                    t++;
+                    if (moveto[t] == 2)
+                    {
+                        locationx = savelx + (subfromsize / 2) * whichpart +380;
+                        for (int p = 0; p < whichpart * 2; p = p + 2)
+                        {
+                            if (whereisthetowerpart[p] == moveto[t])
+                            {
+                                height++;
+                            }
+                        }
+                        locationy = savely - sizey * height;
+                        if (whichpart < towerpic.Length)
+                        {
+                            towerpic[whichpart].Location = new Point(locationx, locationy);
+                        }
+                        t++;
+                          if (t == moveto.Length)
+                            this.Controls.Remove(button2);
+                        return;
+                    }
+                    if (moveto[t] == 1)
+                    {
+                        locationx = savelx + (subfromsize / 2) * whichpart;
+                        for (int p = 0; p < whichpart * 2; p = p + 2)
+                        {
+                            if (whereisthetowerpart[p] == moveto[t])
+                            {
+                                height++;
+                            }
+                        }
+                        locationy = savely - sizey * height;
+                        if (whichpart < towerpic.Length)
+                        {
+                            towerpic[whichpart].Location = new Point(locationx, locationy);
+                        }
+                        t++;
+                        if (t == moveto.Length)
+                            this.Controls.Remove(button2);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowIntBoard(int[] a)
+        {
+            string s = "";
+            int i;
+            for (i = 0; i < a.Length; i++)
+            {
+                s = s + a[i].ToString() + "\t";
+            }
+            MessageBox.Show(s);
+        }
+        private void SolveHanoi(int a, int b, int c, int n)
+        {
+            if (n == 1)
+            {
+
+                Array.Resize(ref moveto, moveto.Length + 2);
+                moveto[i] = a;
+                moveto[i + 1] = c;
+                i = i + 2;
+            }
+            else
+            {
+                SolveHanoi(a, c, b, n - 1);
+                Array.Resize(ref moveto, moveto.Length + 2);
+                moveto[i] = a;
+                moveto[i + 1] = c;
+                i = i + 2;
+                SolveHanoi(b, a, c, n - 1);
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string t = textBox1.Text;
+                l = int.Parse(t);
+            }
+            catch
+            {
+
+            }
+            if (l > 2 && l < 10)
+            {
+                g++;
+                if (g == 1)
+                {
+                    try
+                    {
+                        string s = textBox1.Text;
+                        size = int.Parse(s);
+                        tower = new int[size];
+                        towerpic = new Panel [size];
+                        whereisthetowerpart = new int[size * 2];
+                        savelx = locationx;
+                        savely = locationy;
+                        subfromsize = (sizex - 50) / size;                        
+                        int num = 0;
+                        for (int p  = 0, o=0; p < size; p++, sizex =sizex-subfromsize, locationy=locationy-sizey, locationx=locationx+(subfromsize/2),num++)
+                        {
+                            whereisthetowerpart[o] = 1;
+                            o++;
+                            whereisthetowerpart[o] = num;
+                            o++;
+                            towerpic[p] = new Panel();
+                            towerpic[p].Size = new Size(sizex,sizey );
+                            towerpic[p].Location = new Point(locationx, locationy);
+                            towerpic[p].BackColor = Color.Transparent;
+                            towerpic[p].BackgroundImage = player;
+                            towerpic[p].BackgroundImageLayout = ImageLayout.Stretch;
+                            this.Controls.Add(towerpic[p]);
+                        }
+                        moveto = new int[i]; 
+                        SolveHanoi(size);
+                        this.Controls.Remove(textBox1);
+                        this.Controls.Remove(button1);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("ERROR");
+                    }
+                }
+            }     
+        }
+    }
+}
